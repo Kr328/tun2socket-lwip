0b98e8970638417e16d032e79a3da5727b8c57d7
Kr328 <kr328app@outlook.com>
2021-05-20 11:40:15 +0800

    Feature: add cross compile support

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,13 +1,17 @@
 package bridge
 
-//go:generate cmake -E make_directory build
-//go:generate cmake -E chdir build cmake $CMAKE_ARGS ../native
-//go:generate cmake -E chdir build cmake --build .
-//go:generate go run timestamp/build.go build/libnative.a build/liblwip.a build.go
+//go:generate cmake -E make_directory build/$GOARCH
+//go:generate cmake -E chdir build/$GOARCH cmake $CMAKE_ARGS ../../native
+//go:generate cmake -E chdir build/$GOARCH cmake --build .
+//go:generate go run timestamp/build.go build/$GOARCH/libnative.a build/$GOARCH/liblwip.a build.go
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/native
-#cgo LDFLAGS: -L${SRCDIR}/build -lnative -llwip
+
+#cgo amd64 LDFLAGS: -L${SRCDIR}/build/amd64 -lnative -llwip
+#cgo 386 LDFLAGS: -L${SRCDIR}/build/386 -lnative -llwip
+#cgo arm64 LDFLAGS: -L${SRCDIR}/build/arm64 -lnative -llwip
+#cgo arm LDFLAGS: -L${SRCDIR}/build/arm -lnative -llwip
 
 #include "init.h"
 */
