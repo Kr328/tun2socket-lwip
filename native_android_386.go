4fd5149976afdbd4aa289f599d16fd3b2997d026
Kr328 <kr328app@outlook.com>
2021-09-10 16:32:34 +0800

    Feature: add prebuilt binaries

---
+++ native_android_386.go
@@ -0,0 +1,14 @@
+// Code generated .* DO NOT EDIT.
+
+//go:build android && 386
+// +build android,386
+
+package tun2socket
+
+/*
+#cgo CFLAGS: -Inative -fPIC -O3
+#cgo LDFLAGS: -lnative_android_386
+*/
+import "C"
+
+const BuildNativeMD5 = "16cdad6cacefe727095a0b55a3921a5b"
---
+++ native_android_arm.go
@@ -0,0 +1,14 @@
+// Code generated .* DO NOT EDIT.
+
+//go:build android && arm
+// +build android,arm
+
+package tun2socket
+
+/*
+#cgo CFLAGS: -Inative -fPIC -O3
+#cgo LDFLAGS: -lnative_android_arm
+*/
+import "C"
+
+const BuildNativeMD5 = "bdf7ccc6f9acf724dca0b2f40d27cbda"
