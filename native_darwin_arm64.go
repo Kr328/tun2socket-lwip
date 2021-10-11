c693235d3187aa0e22235835999983342f223663
Kr328 <kr328app@outlook.com>
2021-10-11 18:08:11 +0800

    Chore: add macos/arm64 build

---
+++ native_darwin_arm64.go
@@ -0,0 +1,14 @@
+// Code generated .* DO NOT EDIT.
+
+//go:build darwin && arm64
+// +build darwin,arm64
+
+package tun2socket
+
+/*
+#cgo CFLAGS: -Inative -fPIC -O3
+#cgo LDFLAGS: -L. -lnative_darwin_arm64
+*/
+import "C"
+
+const BuildNativeMD5 = "a4552bc9dee064d16292cb1fe618a284"
